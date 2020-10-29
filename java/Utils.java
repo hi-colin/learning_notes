e0c66a4df51c2b2b2f0cc374e72c51c0a6248fc9
Colin <811687790@qq.com>
2020-10-29 10:30:36 +0800

    add utils

---
+++ java/Utils.java
@@ -0,0 +1,18 @@
+package java;
+
+class Utils {
+
+    public void util() {
+        // 字符串与整型的相互转换
+        String str = String.valueOf(2);
+        int i = Integer.parseInt(str);
+
+        // 获取当前方法的名字
+        String methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+        // 字符串转日期
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = format.parse(str);
+    }
+
+}
